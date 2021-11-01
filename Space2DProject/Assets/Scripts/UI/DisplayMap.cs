b4c5812725f5abebf2bf78601c55865d7a4bee05
HubertBagarre <h.ramiandrasoa@rubika-edu.com>
2021-11-01 22:38:46 +0100

    savepoint

--- Space2DProject/Assets/Scripts/UI/DisplayMap.cs
+++ Space2DProject/Assets/Scripts/UI/DisplayMap.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class DisplayMap : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private GameObject map;
+    private bool isActive = false;
+    
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            isActive = !isActive;
+            map.SetActive(isActive);
+        }
     }
 }
