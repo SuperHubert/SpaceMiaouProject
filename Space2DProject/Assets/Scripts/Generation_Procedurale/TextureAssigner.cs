a5420a05860768d757911cff2258528dcba32058
HubertBagarre <h.ramiandrasoa@rubika-edu.com>
2021-11-11 22:26:08 +0100

    save

--- Space2DProject/Assets/Scripts/Generation_Procedurale/TextureAssigner.cs
+++ Space2DProject/Assets/Scripts/Generation_Procedurale/TextureAssigner.cs
@@ -78,7 +78,18 @@ public class TextureAssigner : MonoBehaviour
     [SerializeField] private List<GameObject> b3Ver14;
     
     private List<GameObject>[,] prefabGrid = new List<GameObject>[3,15];
-    
+
+    public void Start()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 15; j++)
+            {
+                prefabGrid[i, j] = new List<GameObject>();
+            }
+        }
+    }
+
     public void FillAllPools()
     {
         for (int i = 0; i < 3; i++)
@@ -86,15 +97,6 @@ public class TextureAssigner : MonoBehaviour
             for (int j = 0; j < 15; j++)
             {
                 RefillPool(i,j);
-            }
-        }
-
-        Debug.Log("grid state");
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 15; j++)
-            {
-                Debug.Log(prefabGrid[i,j][0]);
             }
         }
     }
@@ -107,63 +109,63 @@ public class TextureAssigner : MonoBehaviour
                 switch (n)
                 {
                     case 0:
-                        prefabGrid[0, 0] = b1Ver4;
+                        RefillList(prefabGrid[0, 0],b1Ver4);
                         break;
                 
                     case 1:
-                        prefabGrid[0, 1] = b1Ver31;
+                        RefillList(prefabGrid[0, 1],b1Ver31);
                         break;
                 
                     case 2:
-                        prefabGrid[0, 2] = b1Ver32;
+                        RefillList(prefabGrid[0, 2] ,b1Ver32);
                         break;
                 
                     case 3:
-                        prefabGrid[0, 3] = b1Ver33;
+                        RefillList(prefabGrid[0, 3] ,b1Ver33);
                         break;
                 
                     case 4:
-                        prefabGrid[0, 4] = b1Ver34;
+                        RefillList(prefabGrid[0, 4] ,b1Ver34);
                         break;
                 
                     case 5:
-                        prefabGrid[0, 5] = b1Ver21;
+                        RefillList(prefabGrid[0, 5] ,b1Ver21);
                         break;
                 
                     case 6:
-                        prefabGrid[0, 6] = b1Ver22;
+                        RefillList(prefabGrid[0, 6] ,b1Ver22);
                         break;
                 
                     case 7:
-                        prefabGrid[0, 7] = b1Ver23;
+                        RefillList(prefabGrid[0, 7] ,b1Ver23);
                         break;
                 
                     case 8:
-                        prefabGrid[0, 8] = b1Ver24;
+                        RefillList(prefabGrid[0, 8] ,b1Ver24);
                         break;
                 
                     case 9:
-                        prefabGrid[0, 9] = b1Ver25;
+                        RefillList(prefabGrid[0, 9] ,b1Ver25);
                         break;
                 
                     case 10:
-                        prefabGrid[0, 10] = b1Ver26;
+                        RefillList(prefabGrid[0, 10],b1Ver26);
                         break;
                 
                     case 11:
-                        prefabGrid[0, 11] = b1Ver11;
+                        RefillList(prefabGrid[0, 11],b1Ver11);
                         break;
                 
                     case 12:
-                        prefabGrid[0, 12] = b1Ver12;
+                        RefillList(prefabGrid[0, 12],b1Ver12);
                         break;
                 
                     case 13:
-                        prefabGrid[0, 13] = b1Ver13;
+                        RefillList(prefabGrid[0, 13],b1Ver13);
                         break;
                 
                     case 14:
-                        prefabGrid[0, 14] = b1Ver14;
+                        RefillList(prefabGrid[0, 14],b1Ver14);
                         break;
                 }
 
@@ -172,63 +174,63 @@ public class TextureAssigner : MonoBehaviour
                 switch (n)
                 {
                     case 0:
-                        prefabGrid[1, 0] = b2Ver4;
+                        RefillList(prefabGrid[1, 0], b2Ver4);
                         break;
                 
                     case 1:
-                        prefabGrid[1, 1] = b2Ver31;
+                        RefillList(prefabGrid[1, 1] ,b2Ver31);
                         break;
                 
                     case 2:
-                        prefabGrid[1, 2] = b2Ver32;
+                        RefillList(prefabGrid[1, 2] ,b2Ver32);
                         break;
                 
                     case 3:
-                        prefabGrid[1, 3] = b2Ver33;
+                        RefillList(prefabGrid[1, 3] ,b2Ver33);
                         break;
                 
                     case 4:
-                        prefabGrid[1, 4] = b2Ver34;
+                        RefillList(prefabGrid[1, 4] ,b2Ver34);
                         break;
                 
                     case 5:
-                        prefabGrid[1, 5] = b2Ver21;
+                        RefillList(prefabGrid[1, 5] ,b2Ver21);
                         break;
                 
                     case 6:
-                        prefabGrid[1, 6] = b2Ver22;
+                        RefillList(prefabGrid[1, 6] ,b2Ver22);
                         break;
                 
                     case 7:
-                        prefabGrid[1, 7] = b2Ver23;
+                        RefillList(prefabGrid[1, 7] ,b2Ver23);
                         break;
                 
                     case 8:
-                        prefabGrid[1, 8] = b2Ver24;
+                        RefillList(prefabGrid[1, 8] ,b2Ver24);
                         break;
                 
                     case 9:
-                        prefabGrid[1, 9] = b2Ver25;
+                        RefillList(prefabGrid[1, 9] ,b2Ver25);
                         break;
                 
                     case 10:
-                        prefabGrid[1, 10] = b2Ver26;
+                        RefillList(prefabGrid[1, 10],b2Ver26);
                         break;
                 
                     case 11:
-                        prefabGrid[1, 11] = b2Ver11;
+                        RefillList(prefabGrid[1, 11],b2Ver11);
                         break;
                 
                     case 12:
-                        prefabGrid[1, 12] = b2Ver12;
+                        RefillList(prefabGrid[1, 12],b2Ver12);
                         break;
                 
                     case 13:
-                        prefabGrid[1, 13] = b2Ver13;
+                        RefillList(prefabGrid[1, 13],b2Ver13);
                         break;
                 
                     case 14:
-                        prefabGrid[1, 14] = b2Ver14;
+                        RefillList(prefabGrid[1, 14],b2Ver14);
                         break;
                 }
 
@@ -237,63 +239,63 @@ public class TextureAssigner : MonoBehaviour
                 switch (n)
                 {
                     case 0:
-                        prefabGrid[2, 0] = b3Ver4;
+                         RefillList(prefabGrid[2, 0] ,b3Ver4);
                         break;
                 
                     case 1:
-                        prefabGrid[2, 1] = b3Ver31;
+                        RefillList(prefabGrid[2, 1] ,b3Ver31);
                         break;
                 
                     case 2:
-                        prefabGrid[2, 2] = b3Ver32;
+                        RefillList(prefabGrid[2, 2] ,b3Ver32);
                         break;
                 
                     case 3:
-                        prefabGrid[2, 3] = b3Ver33;
+                        RefillList(prefabGrid[2, 3] ,b3Ver33);
                         break;
                 
                     case 4:
-                        prefabGrid[2, 4] = b3Ver34;
+                        RefillList(prefabGrid[2, 4] ,b3Ver34);
                         break;
                 
                     case 5:
-                        prefabGrid[2, 5] = b3Ver21;
+                        RefillList(prefabGrid[2, 5] ,b3Ver21);
                         break;
                 
                     case 6:
-                        prefabGrid[2, 6] = b3Ver22;
+                        RefillList(prefabGrid[2, 6] ,b3Ver22);
                         break;
                 
                     case 7:
-                        prefabGrid[2, 7] = b3Ver23;
+                        RefillList(prefabGrid[2, 7] ,b3Ver23);
                         break;
                 
                     case 8:
-                        prefabGrid[2, 8] = b3Ver24;
+                        RefillList(prefabGrid[2, 8] ,b3Ver24);
                         break;
                 
                     case 9:
-                        prefabGrid[2, 9] = b3Ver25;
+                        RefillList(prefabGrid[2, 9] ,b3Ver25);
                         break;
                 
                     case 10:
-                        prefabGrid[2, 10] = b3Ver26;
+                        RefillList(prefabGrid[2, 10],b3Ver26);
                         break;
                 
                     case 11:
-                        prefabGrid[2, 11] = b3Ver11;
+                        RefillList(prefabGrid[2, 11],b3Ver11);
                         break;
                 
                     case 12:
-                        prefabGrid[2, 12] = b3Ver12;
+                        RefillList(prefabGrid[2, 12],b3Ver12);
                         break;
                 
                     case 13:
-                        prefabGrid[2, 13] = b3Ver13;
+                        RefillList(prefabGrid[2, 13],b3Ver13);
                         break;
                 
                     case 14:
-                        prefabGrid[2, 14] = b3Ver14;
+                        RefillList(prefabGrid[2, 14],b3Ver14);
                         break;
                 }
 
@@ -301,11 +303,19 @@ public class TextureAssigner : MonoBehaviour
         }
     }
 
+    private void RefillList(List<GameObject> target, List<GameObject> origin)
+    {
+        target.Clear();
+        foreach (GameObject item in origin)
+        {
+            target.Add(item);
+        }
+    }
+
     public GameObject GetRoom(bool isTop, bool isBot, bool isLeft, bool isRight)
     {
         int currentRegion = LevelManager.Instance.GetBiome();
-        Debug.Log("Region : "+currentRegion);
-        
+
         int a = 1;
         int b = 1;
         int c = 1;
@@ -358,67 +368,52 @@ public class TextureAssigner : MonoBehaviour
             {
                 
                 case 2 * 3 * 5 * 7: //210
-                    CheckRefill(currentRegion,0);
-                    return prefabGrid[currentRegion, 0][Random.Range(0, prefabGrid[currentRegion, 0].Count)];
+                    return PickRoom(currentRegion,0);
                 
                 case 2 * 5 * 7: //70
-                    CheckRefill(currentRegion,1);
-                    return prefabGrid[currentRegion, 1][Random.Range(0, prefabGrid[currentRegion, 1].Count)];
+                    return PickRoom(currentRegion,1);
                 
                 case 3 * 5 * 7: //105
-                    CheckRefill(currentRegion,2);
-                    return prefabGrid[currentRegion, 2][Random.Range(0, prefabGrid[currentRegion, 2].Count)];
+                    return  PickRoom(currentRegion,2);
                 
                 case 2 * 3 * 7: //42
-                    CheckRefill(currentRegion,3);
-                    return prefabGrid[currentRegion, 3][Random.Range(0, prefabGrid[currentRegion, 3].Count)];
+                    return  PickRoom(currentRegion,3);
                 
                 case 2 * 3 * 5: //30
-                    CheckRefill(currentRegion,4);
-                    return prefabGrid[currentRegion, 4][Random.Range(0, prefabGrid[currentRegion, 4].Count)];
+                    return  PickRoom(currentRegion,4);
 
                 case 2 * 3: //6
-                    CheckRefill(currentRegion,5);
-                    return prefabGrid[currentRegion, 5][Random.Range(0, prefabGrid[currentRegion, 5].Count)];
+                    return  PickRoom(currentRegion,5);
 
                 case 5 * 7: //35
-                    CheckRefill(currentRegion,6);
-                    return prefabGrid[currentRegion, 6][Random.Range(0, prefabGrid[currentRegion, 6].Count)];
+                    return  PickRoom(currentRegion,6);
 
                 case 2 * 7: //14
-                    CheckRefill(currentRegion,7);
-                    return prefabGrid[currentRegion, 7][Random.Range(0, prefabGrid[currentRegion, 7].Count)];
+                    return  PickRoom(currentRegion,7);
 
                 case 2 * 5: //10
-                    CheckRefill(currentRegion,8);
-                    return prefabGrid[currentRegion, 8][Random.Range(0, prefabGrid[currentRegion, 8].Count)];
+                    return  PickRoom(currentRegion,8);
 
                 case 3 * 5: //15
-                    CheckRefill(currentRegion,9);
-                    return prefabGrid[currentRegion, 9][Random.Range(0, prefabGrid[currentRegion, 9].Count)];
+                    return  PickRoom(currentRegion,9);
 
                 case 3 * 7: //21
-                    CheckRefill(currentRegion,10);
-                    return prefabGrid[currentRegion, 10][Random.Range(0, prefabGrid[currentRegion, 10].Count)];
+                     return PickRoom(currentRegion,10);
 
                 case 2:
-                    CheckRefill(currentRegion,11);
-                    return prefabGrid[currentRegion, 11][Random.Range(0, prefabGrid[currentRegion, 11].Count)];
+                     return PickRoom(currentRegion,11);
 
                 case 7:
-                    CheckRefill(currentRegion,12);
-                    return prefabGrid[currentRegion, 12][Random.Range(0, prefabGrid[currentRegion, 12].Count)];
+                     return PickRoom(currentRegion,12);
 
                 case 3:
-                    CheckRefill(currentRegion,13);
-                    return prefabGrid[currentRegion, 13][Random.Range(0, prefabGrid[currentRegion, 13].Count)];
+                     return PickRoom(currentRegion,13);
 
                 case 5:
-                    CheckRefill(currentRegion,14);
-                    return prefabGrid[currentRegion, 14][Random.Range(0, prefabGrid[currentRegion, 14].Count)];
+                     return PickRoom(currentRegion,14);
 
                 default:
-                    return prefabGrid[currentRegion, 0][Random.Range(0, prefabGrid[currentRegion, 0].Count)];
+                    return PickRoom(0,0);;
                     
             }
         }
@@ -426,15 +421,33 @@ public class TextureAssigner : MonoBehaviour
 
     private void CheckRefill(int b, int n)
     {
-        /*
-        if (prefabGrid[b, n].Count < 1)
+        Debug.Log("PrefabGrid["+b+","+n+"].Count="+prefabGrid[b, n].Count);
+        if (prefabGrid[b, n].Count == 0)
         {
             RefillPool(b, n);
         }
-        */
+        
     }
-    
-    
+
+    private GameObject PickRoom(int region, int n)
+    {
+        CheckRefill(region,n);
+
+        int a = prefabGrid[region, n].Count - 1;
+        
+        Debug.Log("a = "+a);
+        foreach (var VARIABLE in prefabGrid[region,n])
+        {
+            Debug.Log(VARIABLE);
+            
+        }
+        
+        GameObject target = prefabGrid[region, 0][Random.Range(0,a)];
+        
+        prefabGrid[region, 0].Remove(target);
+
+        return target;
+    }
     
 
 
