4fb647920815462db5560b6d1d4c161fb684b6e5
SuperHubert <h.ramiandrasoa@rubika-edu.com>
2021-12-02 12:36:01 +0100

    added Spyke Trap

---
+++ Space2DProject/Assets/Scripts/Enemy/Traps/Spikes.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spikes : MonoBehaviour
+{
+    [SerializeField] private int damage = 1;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        //play Animation
+        LifeManager.Instance.TakeDamages(damage);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        //play Animation
+    }
+}
