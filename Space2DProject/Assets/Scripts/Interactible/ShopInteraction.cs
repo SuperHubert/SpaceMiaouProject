e3c9b2f272454b5f4efc23820047a47a8ecb735a
Noxxeur <80094950+Noxxeur@users.noreply.github.com>
2021-12-03 17:26:27 +0100

    prix et noms des items dans le shop

--- Space2DProject/Assets/Scripts/Interactible/ShopInteraction.cs
+++ Space2DProject/Assets/Scripts/Interactible/ShopInteraction.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ShopInteraction : MonoBehaviour, IInteractible
 {
@@ -9,11 +10,25 @@ public class ShopInteraction : MonoBehaviour, IInteractible
     public ShopManager shopManager;
     public Button exitButton;
 
+    public List<TextMeshProUGUI> textList;
+    public List<TextMeshProUGUI> testNameList;
 
     public void OnInteraction()
     {
         shopUI.SetActive(true);
         Time.timeScale = 0;
+
+        var displayList = shopManager.DisplayItems(0,1,5);
+
+        //var displayList = shopManager.DisplayItems(LevelManager.Instance.GetCurrentFloorNumber(), LevelManager.Instance.GetCurrentSeed(), 5);
+
+        for (int i = 0; i < textList.Count; i++)
+        {
+            displayList[i].actualPrice = displayList[i].basePrice - shopManager.reductionTotal;
+            textList[i].text = displayList[i].actualPrice.ToString();
+            testNameList[i].text = displayList[i].name;
+
+        }
     }
 
     public void CloseShop()
@@ -21,4 +36,6 @@ public class ShopInteraction : MonoBehaviour, IInteractible
         shopUI.SetActive(false);
         Time.timeScale = 1;
     }
+
+    
 }
--- Space2DProject/Assets/Scripts/Shop/ShopManager.cs
+++ Space2DProject/Assets/Scripts/Shop/ShopManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 
 public class ShopManager : MonoBehaviour
 {
-    public float reductionTotal = 0;
+    public int reductionTotal = 0;
 
     #region Singleton
     public static ShopManager Instance;
@@ -17,30 +17,20 @@ public class ShopManager : MonoBehaviour
     }
     #endregion
 
-    [System.Serializable] class ShopItem
+    [System.Serializable] public class ShopItem
     {
-        [SerializeField] private UnityEvent upgrade = new UnityEvent();
+        public string name;
         public Sprite Image;
         public int basePrice;
-        private int actualPrice;
+        public int actualPrice;
+        [SerializeField] private UnityEvent upgrade = new UnityEvent();
+
     }
 
-    [SerializeField] List<ShopItem> ShopItemList;
-    List<ShopItem> ShopItemList2;
+    [SerializeField] List<ShopItem> ShopItemList = new List<ShopItem>();
+    [SerializeField] List<ShopItem> ShopItemList2 = new List<ShopItem>();
 
-    private void Start()
-    {
-        DuplicateList();
-    }
-
-    public void DuplicateList()
-    {
-        ShopItemList2 = ShopItemList;
-    }
-
-
-
-    public void ReductionPickI()
+     public void ReductionPickI()
     {
         reductionTotal ++;
         Debug.Log("Reduction +1");
@@ -49,10 +39,55 @@ public class ShopManager : MonoBehaviour
 
     public void ReductionPickII()
     {
-        reductionTotal++;
+        reductionTotal ++;
         Debug.Log("Reduction +2");
 
     }
+
+    public List<ShopItem> DisplayItems(int floor, int seed, int size)
+    {
+        ShopItemList2.Clear();
+        foreach(var item in ShopItemList)
+        {
+            ShopItemList2.Add(item);
+        }
+
+
+        List<ShopItem> returnList = new List<ShopItem>();
+
+        Random.InitState(seed + floor);
+
+        for (int i = 0; i < floor+1; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (ShopItemList2.Count <= 0)
+                {
+                    foreach (var item in ShopItemList)
+                    {
+                        ShopItemList2.Add(item);
+                    }
+
+                }
+
+                var addedItem = ShopItemList2[Random.Range(0, ShopItemList2.Count)];
+
+                returnList.Add(addedItem);
+
+                ShopItemList2.Remove(addedItem);
+            }
+
+            if(i != floor)
+            {
+                returnList.Clear();
+            }
+            
+        }
+
+        return returnList;
+    }
+
+
 
 
 }
