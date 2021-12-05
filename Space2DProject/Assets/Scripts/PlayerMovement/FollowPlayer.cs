4f9c3967f3ab5e7df8164a60c9fd7321f3f372e2
HubertBagarre <h.ramiandrasoa@rubika-edu.com>
2021-12-05 17:26:57 +0100

    Simple player follow (ld needs tilemap on void)

---
+++ Space2DProject/Assets/Scripts/PlayerMovement/FollowPlayer.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    [SerializeField] private Transform player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+}
