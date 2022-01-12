8bb3c7fdf9545fabbfe8da2031a1fb96ca7e4a3b
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-12 01:26:15 +0100

    Better teleport animation in hub

--- Space2DProject/Assets/Scripts/Interactible/StartGame.cs
+++ Space2DProject/Assets/Scripts/Interactible/StartGame.cs
@@ -6,32 +6,22 @@ using UnityEngine.SceneManagement;
 public class StartGame : MonoBehaviour, IInteractible
 {
     [SerializeField] private GameObject player;
-    [SerializeField] GameObject rayObj;
-    [SerializeField] Transform tpPos;
+    [SerializeField] private Animator animator;
     [SerializeField] Transform parent;
     
     public void OnInteraction()
     {
         if(parent != null) transform.SetParent(parent);
+        animator.SetTrigger("Trigger");
         StartCoroutine(AnimationRoutine());
     }
 
+    public float value;
     IEnumerator AnimationRoutine()
     {
-        player.transform.position = tpPos.transform.position;
         player.SetActive(false);
-        tpPos.gameObject.SetActive(true);
-        rayObj.SetActive(true);
-        
-        yield return new WaitForSeconds(0.11f);
-        
-        tpPos.gameObject.SetActive(false);
-
-        yield return new WaitForSeconds(1.22f);
-        
-        rayObj.SetActive(false);
+        yield return new WaitForSeconds(1f);
         LoadingManager.Instance.canvas.SetActive(true);
-        yield return new WaitForSeconds(0.05f);
-        SceneManager.LoadScene(4);
+        //SceneManager.LoadScene(4);
     }
 }
