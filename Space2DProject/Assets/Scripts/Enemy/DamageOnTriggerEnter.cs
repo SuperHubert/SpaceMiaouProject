930d9330099bb29ea3764a897f97d8a93c281b6a
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-13 18:30:02 +0100

    Poison Puddle done

--- Space2DProject/Assets/Scripts/Enemy/DamageOnTriggerEnter.cs
+++ Space2DProject/Assets/Scripts/Enemy/DamageOnTriggerEnter.cs
@@ -1,11 +1,42 @@
+using System;
 using UnityEngine;
 public class DamageOnTriggerEnter : MonoBehaviour
 {
     public int damage = 1;
-    
+    public bool feetHitbox = false;
+    public float hitboxMove;
+    public float detectOffset;
+    public float moveOffset;
+    private Transform playerTransform;
+    private BoxCollider2D collider;
+    private float yPos;
+
+    private void Start()
+    {
+        playerTransform = LevelManager.Instance.Player().transform;
+        collider = gameObject.GetComponent<BoxCollider2D>();
+        yPos = collider.offset.y;
+    }
+
+    private void Update()
+    {
+        if(!feetHitbox) return;
+        if (playerTransform.position.y < transform.position.y + detectOffset)
+        {
+            collider.offset = Vector2.right * yPos + Vector2.up * moveOffset;
+        }
+        else
+        {
+            collider.offset = Vector2.right * yPos;
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.layer != 6) return;
         LifeManager.Instance.TakeDamages(damage);
     }
+    
+    
+    
 }
