55736a1f319fa27beee01818e94610f55c89b4db
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-15 22:36:08 +0100

    script creep done (kinda)

--- Space2DProject/Assets/Scripts/Enemy/UpdatedEnemyBehaviours/BigWalkTowardsBehaviour.cs
+++ Space2DProject/Assets/Scripts/Enemy/UpdatedEnemyBehaviours/BigWalkTowardsBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BigWalkTowardsBehaviour : EnemyBehaviour
 {
+    [SerializeField] private GameObject ondeDeChoc;
+
     private void Update()
     {
         if(currentState != State.Awake) return;
@@ -21,20 +23,35 @@ public class BigWalkTowardsBehaviour : EnemyBehaviour
         
         if (currentState == State.Awake && !isPerformingAction)
         {
-            agent.SetDestination(player.position);
+            if(agent.isOnNavMesh) agent.SetDestination(player.position);
         }
     }
 
     protected override void Action()
     {
         base.Action();
-        //StartCoroutine(Attack());
+        
+        StartCoroutine(Attack());
     }
 
-    /*
+    
     private IEnumerator Attack()
     {
+        if(agent.isOnNavMesh) agent.SetDestination(enemyTransform.position);
+        yield return new WaitUntil(() => !ondeDeChoc.activeSelf);
+        //leve la tete et la frappe sur le sol
         
+        // changer tout les animator. pour coller avec l'animator
+        animator.enabled = true;
+        animator.Rebind();
+        animator.Update(0f);
+        yield return new WaitForSeconds(0.833f);
+        ondeDeChoc.transform.position = enemyTransform.position;
+        ondeDeChoc.SetActive(true);
+        isPerformingAction = false;
+        animator.enabled = false;
+        yield return new WaitForSeconds(2f);
+        ondeDeChoc.SetActive(false);
     }
-    */
+    
 }
