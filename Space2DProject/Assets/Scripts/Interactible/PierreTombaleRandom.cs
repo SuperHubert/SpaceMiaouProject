95545c00c9a3489fcd7672a84ebc55648026894b
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-16 20:25:16 +0100

    Script Pierre Tombale Random Done

---
+++ Space2DProject/Assets/Scripts/Interactible/PierreTombaleRandom.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class PierreTombaleRandom : MonoBehaviour,IInteractible
+{
+    public List<Dialogues> dialogues;
+    public static List<Dialogues> usableDialogues = new List<Dialogues>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (usableDialogues.Count != 0) return;
+        RefillDialogues();
+    }
+    
+    private void RefillDialogues()
+    {
+        usableDialogues.Clear();
+        foreach (var dialogue in dialogues)
+        {
+            usableDialogues.Add(dialogue);
+        }
+    }
+
+    public void OnInteraction()
+    {
+        if (usableDialogues.Count == 0) RefillDialogues();
+        var dialogue = usableDialogues[Random.Range(0, usableDialogues.Count)];
+        usableDialogues.Remove(dialogue);
+        gameObject.GetComponent<Collider2D>().enabled = false;
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+}
