e80392f4591e49ca7b373cbef036542a63724a32
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-17 19:37:15 +0100

    Hand script done

---
+++ Space2DProject/Assets/Scripts/Enemy/Traps/Hand.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    [SerializeField] private Collider2D col;
+    private GameObject player;
+    private LifeManager lifeManager;
+
+    private void Start()
+    {
+        player = LevelManager.Instance.Player();
+        lifeManager = LifeManager.Instance;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(GrabAnimation());
+    }
+
+    IEnumerator GrabAnimation()
+    {
+        animator.SetTrigger("Trigger");
+        player.SetActive(lifeManager.canTakeDamge = InputManager.canInput = col.enabled = false);
+        yield return new WaitForSeconds(1f);
+        player.transform.position = transform.position + new Vector3(0.111f,-0.817f,0);
+        player.SetActive(lifeManager.canTakeDamge = InputManager.canInput = true);
+        lifeManager.TakeDamages(1);
+        yield return new WaitForSeconds(1f);
+        col.enabled = true;
+
+    }
+}
