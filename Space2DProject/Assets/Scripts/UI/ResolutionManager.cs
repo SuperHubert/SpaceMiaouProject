d163b786ed860447f4605834f2dfc6cacabaed67
Super Hubert <h.ramiandrasoa@rubika-edu.com>
2022-01-19 16:00:30 +0100

    Fix Resolution

--- Space2DProject/Assets/Scripts/UI/ResolutionManager.cs
+++ Space2DProject/Assets/Scripts/UI/ResolutionManager.cs
@@ -2,20 +2,13 @@ using UnityEngine;
 
 public class ResolutionManager : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
         ResetRes();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    
     public void ResetRes()
     {
-        Screen.SetResolution(1280,720,true);
+        Screen.SetResolution(1920,1080,FullScreenMode.MaximizedWindow);
     }
 }
